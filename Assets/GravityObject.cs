de0c988f6cce96743bac1d4a0aa34a514701c793
Christian Loder <Christian.Loder@Knapp.com>
2017-01-25 12:01:40 +0100

    Initial Commit to backup little code

---
+++ Assets/GravityObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class GravityObject : MonoBehaviour
+{
+	public GameObject planet; // assign your planet GO in unity editor here
+
+	public float distanceMassLimit = 1f;
+	public bool fixInSpace = false;
+	public const float gravityConstant = 6.67f;
+
+	public void applyGravity() {
+		if (!fixInSpace) {
+			//get list of Objects nearby, calculate distanceMass factor , apply force when factor is under distanceMassLimit
+			Object[] bodies = GameObject.FindObjectsOfType (typeof(Rigidbody2D));
+			Vector2 forceToApply = Vector2.zero;
+			Rigidbody2D currentBody = GetComponent<Rigidbody2D> ();
+			float dist = 0f;
+
+			foreach (Rigidbody2D body in bodies) {
+				if (body.GetInstanceID () == currentBody.GetInstanceID ())
+					continue;
+				//Debug.Log ("Calculating Force to: " + body.name + " ID: " + body.GetInstanceID ());
+
+				//limit to distanceMassLimit
+				dist = Vector2.Distance(currentBody.position, body.position);
+
+				forceToApply = body.position - currentBody.position;
+				forceToApply *= (gravityConstant * currentBody.mass * body.mass) / Mathf.Pow (dist, 2);
+
+
+			}
+			Debug.Log ("Vector: " + forceToApply.ToString());
+
+			currentBody.AddForce (forceToApply);
+			//Fgrav= (G*m1*m2)/r^2
+			//get the vector to the other object
+			//multiply with calculated force
+			//add to final vector
+
+
+		}
+	}
+}
+
+
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : GravityObject {
+
+	public float gravityFactor = 1f; // then tune this value  in editor too
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Cache the horizontal input.
+		float h = Input.GetAxis("Horizontal");
+
+		GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * 365f);
+
+		//GetComponent<Rigidbody2D>().AddForce((planet.transform.position - transform.position).normalized * GetComponent<Rigidbody2D>().mass * gravityFactor / (planet.transform.position - transform.position).sqrMagnitude);
+
+		applyGravity();
+	}
+
+
+}
